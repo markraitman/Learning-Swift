33c7b9dc4abc085a12cc031c09e4be37a45455ca
markromashkov <markromashkov@mail.ru>
2022-12-06 16:51:58 +0300

    Initial Commit

---
+++ Learning Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Learning Swift
+//
+//  Created by Марк Ромашков on 06.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
